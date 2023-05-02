ea5f960407c896193d12950b50403bfa5a958170
Liza Grossman <lisok.sam@gmail.com>
2023-05-02 18:24:50 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Elizabeth on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
